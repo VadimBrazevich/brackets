4850b4a86818701b04b3fe3b205427b4a404ce02
VadimBrazevich <vadimbrazevich@gmail.com>
2021-02-12 16:12:23 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 != 0) return false;
+  let stack = [];
+  let opend = [];
+  let closed = [];
+  
+  bracketsConfig.forEach(x=>{
+    opend.push(x[0]);
+    closed.push (x[1]);
+  });
+
+  str.split("").forEach(x => {
+
+    if (opend.includes(x) && closed.includes(x)){
+      if (stack[stack.length-1]==x) stack.pop()
+      else stack.push(x)
+    }
+    else if (opend.includes(x)) {
+      stack.push(x);
+    }
+    else{ 
+    let a = stack.pop();
+    let b = opend[closed.indexOf(x)];      
+    if(a != b) {
+        stack.push(a);
+      }
+    } 
+  }); 
+  console.log(stack.length);
+  return stack.length == 0;
 }
